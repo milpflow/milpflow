302d08ddbd2b2df8114b5d8b01258da7666e2ab1
root <outrosdiasvirao@gmail.com>
2014-09-30 11:49:49 -0300

    Update IperfParser

--- IperfParser/src/IperfParser.java
+++ IperfParser/src/IperfParser.java
@@ -6,41 +6,30 @@ import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * IperfParser for Iperf results 
+ * 
+ * How to run:
+ * - 1) Use iperf scripts of MILPFlow project to collect its data
+ * - 2) Run this file to parse the results
+ * - 3) Open a editor to replace the string "0.0- " with "0.0-" (note this this replace is only to remove the space)
+ * - 4) Use Gnumeric to plot its results
+ * 
+ * 
+ * @author Lucio A. Rocha
+ *
+ */
 
 public class IperfParser {
 
-	/*String [] arquivoInput = {
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h18_h29_tcp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h19_h28_tcp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h20_h27_tcp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h21_h26_tcp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h22_h25_tcp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h23_h24_tcp_stp.txt",
-	};*/
-/*	String [] arquivoInput = {
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h18_h29_tcp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h19_h28_tcp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h20_h27_tcp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h21_h26_tcp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h22_h25_tcp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h23_h24_tcp_5m.txt",
-	};
-	*/
-	/*String [] arquivoInput = {
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h18_h29_udp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h19_h28_udp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h20_h27_udp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h21_h26_udp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h22_h25_udp_stp.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/ryu/result_h23_h24_udp_stp.txt",
-	};*/
+	String PATH="/home/lucio/novos_resultados_artigo_im_2015/milpflow/";
 	String [] arquivoInput = {
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h18_h29_udp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h19_h28_udp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h20_h27_udp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h21_h26_udp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h22_h25_udp_5m.txt",
-			"/mnt/compartilhado/artigo_im_2015/results2/milpflow/result_h23_h24_udp_5m.txt",
+			PATH + "nova_result_h18_h29_milpflow_udp.txt",
+			PATH + "nova_result_h19_h28_milpflow_udp.txt",
+			PATH + "nova_result_h20_h27_milpflow_udp.txt",
+			PATH + "nova_result_h21_h26_milpflow_udp.txt",
+			PATH + "nova_result_h22_h25_milpflow_udp.txt",
+			PATH + "nova_result_h23_h24_milpflow_udp.txt"
 	};
 	
 	String [] arquivoOutput = new String [arquivoInput.length];
